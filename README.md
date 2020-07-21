0ecb54e4aa7e1b5440038c3e9f41d575b9b352b5
Benedikt Mandelkow <benedikt.mandelkow@rwth-aachen.de>
2020-07-21 20:53:54 +0200

    port to stm32f103

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 
 ## STM32F303VCT
 `cargo flash --chip STM32F303VCT --example STM32F303VCT --target thumbv7em-none-eabihf`
+
+## STM32F103C8
+`cargo flash --chip STM32F103C8 --example STM32F103C8 --target thumbv7m-none-eabi'
---
+++ examples/STM32F103C8.rs
@@ -0,0 +1,33 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+#![allow(non_snake_case)]
+
+pub use cortex_m::{asm, iprintln};
+use hal::pac as target;
+use panic_semihosting as _;
+use stm32f1xx_hal as hal;
+
+#[rtic::app(device = stm32f1xx_hal::pac, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        itm: target::ITM,
+    }
+    #[init]
+    fn init(cx: init::Context) -> init::LateResources {
+        let cp = cx.core;
+        let mut itm = cp.ITM;
+
+        iprintln!(&mut itm.stim[0], "init");
+        init::LateResources { itm }
+    }
+
+    #[idle(resources = [itm])]
+    fn idle(cx: idle::Context) -> ! {
+        let itm = cx.resources.itm;
+        loop {
+            iprintln!(&mut itm.stim[0], "idle");
+            cortex_m::asm::delay(1000000);
+        }
+    }
+};
